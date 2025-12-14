335a7b258de3f0ac83390a4971c2850863614124
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-12-14 10:19:13 +0700

    Add adjustable grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,39 @@
-const container = document.querySelector(".container");
+const gridInput = document.querySelector("#grid-input");
+const gridContainer = document.querySelector(".container");
 
-const sideGrid = 16;
-const totalGridCount = 16 ** 2;
+const renderGrid = () => {
+  const sideGridCount = gridInput.value;
+  const totalGridCount = sideGridCount ** 2;
+  const gridContainerWidth = 480;
+  const gridWidth = gridContainerWidth / sideGridCount;
 
-for (let i = 0; i < totalGridCount; i++) {
-  const div = document.createElement("div");
-  div.classList.add("pixel");
-  container.appendChild(div);
-}
+  for (let i = 0; i < totalGridCount; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    div.style.width = `${gridWidth}px`;
+    div.style.height = `${gridWidth}px`;
+    gridContainer.appendChild(div);
+  }
+};
+
+renderGrid();
+
+const frame = document.querySelector(".frame");
+const sideGridCountLimit = 100;
+
+const removeGrid = () => {
+  gridContainer.replaceChildren();
+};
+
+frame.addEventListener("change", (ev) => {
+  const target = ev.target;
+
+  switch (target.id) {
+    case "grid-input":
+      if (target.value <= sideGridCountLimit) {
+        removeGrid();
+        renderGrid();
+      }
+      break;
+  }
+});
